53ae7000ff5035aa7072aeca7bb7da9dda840799
percy <murenzipercy@gmail.com>
2025-02-04 13:48:44 +0100

    new change weather

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const apiKey = '4dd5a359ac14211be909b922ae08ce23'; 
-const city = 'Barcelona';
 const weatherInfo = document.getElementById('weather-info');
 
 async function fetchWeather() {
   try {
+    const city = document.getElementById("destination-input").value
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -11,6 +11,7 @@ async function fetchWeather() {
     const data = await response.json();
     const temp = data.main.temp;
     const description = data.weather[0].description;
+    document.getElementById("location").innerText = `Current Weather in ${city}`
     weatherInfo.innerHTML = `<p>Temperature: ${temp}°C</p><p>Condition: ${description}</p>`;
   } catch (error) {
     console.error('Error fetching weather data:', error);
@@ -18,7 +19,11 @@ async function fetchWeather() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', fetchWeather);
+
+const sbtn = document.getElementById("searchBTN")
+sbtn.addEventListener("click", () => {
+  fetchWeather()
+})
 
 function toggleMenu() {
     const navLinks = document.querySelector(".nav-links");
